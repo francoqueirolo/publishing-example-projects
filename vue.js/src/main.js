7853f58bc3645f43fa68fccc6a734b1537cafcc5
Franco Queirolo <fqueiroloc@proveedor.intercorp.com.pe>
2020-03-12 12:32:29 -0500

    feat: web components in basic, angular, vuejs

--- vue.js/src/main.js
+++ vue.js/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { defineCustomElements } from "test-uicmp/loader/";
+
 Vue.config.productionTip = false
+
+defineCustomElements(window);
 
 new Vue({
   render: h => h(App),
